030dd7d58b459b32bbce0e62c3664d1485e20f4b
Rinaldo Medeiros <rinaldo.rmedeiros@gmail.com>
2025-02-11 23:13:39 -0300

    Removendo pedido

--- src/main/java/br/com/fiap/soat8/grp14/techchallenge/presentation/controllers/ProdutoController.java
+++ src/main/java/br/com/fiap/soat8/grp14/techchallenge/presentation/controllers/ProdutoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,6 @@ import br.com.fiap.soat8.grp14.techchallenge.app.dto.produto.ProdutoInsertDTO;
 import br.com.fiap.soat8.grp14.techchallenge.app.services.ProdutoService;
 import br.com.fiap.soat8.grp14.techchallenge.core.entities.enums.CategoriaProduto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
--- src/main/resources/db/migration/V1.1.4__alter_table_pedido_status_pedido.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE public.pedido ADD status_pagamento int2 NULL;
-
-ALTER TABLE public.pedido ADD CONSTRAINT pedido_status_pagamento_check CHECK (((status_pagamento >= 0) AND (status_pagamento <= 3)));
